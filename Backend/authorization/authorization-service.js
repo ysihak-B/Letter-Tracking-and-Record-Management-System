0ecf61bcba816f0f60d73e4dba450d3a13e3fd0f
kidussintayehu <kidussintayehu60@gmail.com>
2023-01-10 16:56:11 +0300

    fix some naming and import issues

--- Backend/authorization/authorization-service.js
+++ Backend/authorization/authorization-service.js
@@ -1,4 +1,3 @@
-const jwt = require("jsonwebtoken");
 const {verifyToken} = require("./verify-token")
 
 const verifyTokenAndAuthorization = (req, res, next) => {
--- Backend/authorization/verify-admin.js
+++ Backend/authorization/verify-admin.js
@@ -1,4 +1,3 @@
-const jwt = require("jsonwebtoken");
 const {verifyToken} = require("./verify-token")
 
 const verifyTokenAndAdmin = (req, res, next) => {
